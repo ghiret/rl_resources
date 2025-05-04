cc758360cd1a86e1672f806f8fb780d31ce2a9ea
Paco Cordobes <ghiret@gmail.com>
2025-05-04 11:34:53 +0000

    feat: Add Grokking Deep Reinforcemente Learning Chapter3 notebook and gym_aima environtment. gym_aima has been adapted to use gymnasium instead of the now deprecated gym.

---
+++ src/envs/gym_aima/__init__.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2018, Miguel Morales
+# Copied from https://github.com/mimoralea/gym-aima/blob/master/gym_aima/envs/__init__.py
+# License: MIT
+"""Gymnasium environments for AIMA."""
