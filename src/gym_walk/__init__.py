7183abc2731d79d9b714c72cf8611c8cf55a7b9e
Paco Cordobes <ghiret@gmail.com>
2025-05-12 06:55:06 +0000

    Initial stab at porting the GDRL Chapter 5 notebook. The MonteCarlo code runs.

--- src/gym_walk/__init__.py
+++ src/gym_walk/__init__.py
@@ -5,7 +5,7 @@ register(
     # five non-terminal states and two terminal
     # same as below
     id="RandomWalk-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 5, "p_stay": 0.0, "p_backward": 0.5},
     max_episode_steps=100,
@@ -16,7 +16,7 @@ register(
     # five non-terminal states and two terminal
     # same as above
     id="RandomWalkFive-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 5, "p_stay": 0.0, "p_backward": 0.5},
     max_episode_steps=100,
@@ -27,7 +27,7 @@ register(
     # Nineteen non-terminal states and two terminal
     # same as below
     id="RandomWalkLarge-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 19, "p_stay": 0.0, "p_backward": 0.5},
     max_episode_steps=100,
@@ -38,7 +38,7 @@ register(
     # Nineteen non-terminal states and two terminal
     # same as above
     id="RandomWalkNineteen-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 19, "p_stay": 0.0, "p_backward": 0.5},
     max_episode_steps=100,
@@ -49,7 +49,7 @@ register(
     # One non-terminal states and two terminal
     # Technically speaking, this is a bandit MDP
     id="BanditWalk-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 1, "p_stay": 0.0, "p_backward": 0.0},
     max_episode_steps=100,
@@ -60,7 +60,7 @@ register(
     # One non-terminal states and two terminal
     # Technically speaking, this is a bandit MDP
     id="BanditDeterministicWalk-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 1, "p_stay": 0.0, "p_backward": 0.0},
     max_episode_steps=100,
@@ -71,7 +71,7 @@ register(
     # One non-terminal states and two terminal
     # Technically speaking, this is a bandit MDP
     id="BanditSlipperyWalk-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 1, "p_stay": 0.0, "p_backward": 0.2},
     max_episode_steps=100,
@@ -82,7 +82,7 @@ register(
 # deterministic transition walks
 register(
     id="WalkThree-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 3, "p_stay": 0.0, "p_backward": 0.0},
     max_episode_steps=100,
@@ -91,7 +91,7 @@ register(
 )
 register(
     id="WalkFive-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 5, "p_stay": 0.0, "p_backward": 0.0},
     max_episode_steps=100,
@@ -100,7 +100,7 @@ register(
 )
 register(
     id="WalkSeven-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 7, "p_stay": 0.0, "p_backward": 0.0},
     max_episode_steps=100,
@@ -109,7 +109,7 @@ register(
 )
 register(
     id="WalkFifthteen-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 15, "p_stay": 0.0, "p_backward": 0.0},
     max_episode_steps=100,
@@ -118,7 +118,7 @@ register(
 )
 register(
     id="WalkSeventeen-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 17, "p_stay": 0.0, "p_backward": 0.0},
     max_episode_steps=1000,
@@ -127,7 +127,7 @@ register(
 )
 register(
     id="WalkNineteen-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 19, "p_stay": 0.0, "p_backward": 0.0},
     max_episode_steps=100,
@@ -138,7 +138,7 @@ register(
 # stochastic transition walks
 register(
     id="SlipperyWalkThree-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 3, "p_stay": 0.5 * 2 / 3.0, "p_backward": 0.5 * 1 / 3.0},
     max_episode_steps=100,
@@ -156,7 +156,7 @@ register(
 )
 register(
     id="SlipperyWalkSeven-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 7, "p_stay": 0.5 * 2 / 3.0, "p_backward": 0.5 * 1 / 3.0},
     max_episode_steps=100,
@@ -165,7 +165,7 @@ register(
 )
 register(
     id="SlipperyWalkFifthteen-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 15, "p_stay": 0.5 * 2 / 3.0, "p_backward": 0.5 * 1 / 3.0},
     max_episode_steps=100,
@@ -174,7 +174,7 @@ register(
 )
 register(
     id="SlipperyWalkSeventeen-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 17, "p_stay": 0.5 * 2 / 3.0, "p_backward": 0.5 * 1 / 3.0},
     max_episode_steps=1000,
@@ -183,7 +183,7 @@ register(
 )
 register(
     id="SlipperyWalkNineteen-v0",
-    entry_point="gym_walk.envs:WalkEnv",
+    entry_point="src.gym_walk.envs:WalkEnv",
     # left-most and right-most states are terminal
     kwargs={"n_states": 19, "p_stay": 0.5 * 2 / 3.0, "p_backward": 0.5 * 1 / 3.0},
     max_episode_steps=100,
